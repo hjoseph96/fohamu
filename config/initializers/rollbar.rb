96d95376535aaf0ba1bc01ed132ac47f28788504
Herbert Joseph <techniquea2z@gmail.com>
2026-07-08 06:46:45 -0400

    fix(secrets): move rollbar into Rails credentials

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -2,7 +2,7 @@ Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
 
-  config.access_token = '03f99f19374444188b83c858d54c467e'
+  config.access_token = Rails.application.credentials.rollbar.access_token
 
   config.enabled = false
 
